7513c051b9d87a6136c5845df78b9f8aa45d90c8
allanromanreyes <allanromanreyes@gmail.com>
2020-06-03 10:28:37 -0700

    First commit

---
+++ covid.py
@@ -0,0 +1,17 @@
+# Coivid tracking data at Pycon 2020 by Matt Harrison
+
+import urllib.request as req
+
+url = 'https://raw.githubusercontent.com/COVID19Tracking/covid-tracking-data/master/data/states_daily_4pm_et.csv'
+
+def fetch_url(url, fname):
+    """
+    Save a URL to a local file
+    """
+    fin = req.urlopen(url)
+    data = fin.read()
+    with open(fname, mode='wb') as fout:
+        fout.write(data)
+
+fname =  'test.csv'
+fetch_url(url, fname)

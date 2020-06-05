f7c2c0c9d1b04a72c73f4c046c4aebb662ae6cd9
allanromanreyes <allanromanreyes@gmail.com>
2020-06-05 22:49:56 +0100

    Third commit

--- covid.py
+++ covid.py
@@ -1,17 +1,12 @@
-# Coivid tracking data at Pycon 2020 by Matt Harrison
+# Pycon2020 Covid tracking tutorial by Matt Harrison
 
+# import and save URL to file
 import urllib.request as req
-
 url = 'https://raw.githubusercontent.com/COVID19Tracking/covid-tracking-data/master/data/states_daily_4pm_et.csv'
-
+fname = 'covid.csv'
 def fetch_url(url, fname):
-    """
-    Save a URL to a local file
-    """
+    """Save a URL to a local file"""
     fin = req.urlopen(url)
     data = fin.read()
     with open(fname, mode='wb') as fout:
         fout.write(data)
-
-fname =  'test.csv'
-fetch_url(url, fname)

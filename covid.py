e658d0d30208d8a54ebc1e0f19e3e9bf10074cc4
allanromanreyes <allanromanreyes@gmail.com>
2020-06-11 23:06:20 -0700

    RPi64

--- covid.py
+++ covid.py
@@ -1,12 +1,12 @@
 # Pycon2020 Covid tracking tutorial by Matt Harrison
-
-# Function to save URL to file
 import urllib.request as req
+
 url = 'https://raw.githubusercontent.com/COVID19Tracking/covid-tracking-data/master/data/states_daily_4pm_et.csv'
-fname = 'covid.csv'
-def fetch_url(url, fname):
+
+def get_url(url, filename):
     """Save a URL to a local file"""
     fin = req.urlopen(url)
     data = fin.read()
-    with open(fname, mode='wb') as fout:
+    with open(filename, mode='wb') as fout:
         fout.write(data)
+
